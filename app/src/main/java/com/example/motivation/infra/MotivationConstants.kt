5b30fc7ea1c968cff59d640488cb21d1f4c9c756
jcssuarez <projetos.informatica@gmail.com>
2022-03-10 22:51:15 -0300

    one

---
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -0,0 +1,19 @@
+package com.example.motivation.infra
+
+class MotivationConstants private constructor(){
+
+    object KEY {
+        val PERSON_NAME = "nome"
+    }private fun handleFilter(){
+
+    }
+        object PHRASEFILTER {
+            val ALL = 1
+            val HAPPY = 2
+            val MORNING = 3
+            val OUTRO = 4
+
+
+        }
+
+}
